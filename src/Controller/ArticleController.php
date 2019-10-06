0c2309207c490f4d60e29ae1449f77a1a4b336cd
Loïc Gallay <lgallay@orange.fr>
2019-10-06 20:23:00 +0200

    Add twig, homepage and show pages

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -6,26 +6,27 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
  * Class ArticleController
  */
-class ArticleController
+class ArticleController extends AbstractController
 {
     /**
-     * @Route("/")
+     * @Route("/", name="app_article_homepage")
      *
      * @return Response
      */
     public function homepage(): Response
     {
-        return new Response('My first page');
+        return $this->render('article/homepage.html.twig');
     }
 
     /**
-     * @Route("/news/{slug}")
+     * @Route("/news/{slug}", name="article_show")
      *
      * @param string $slug
      *
@@ -33,9 +34,15 @@ class ArticleController
      */
     public function show(string $slug)
     {
-        return new Response(sprintf(
-            "Future page to show article : %s",
-            $slug
-        ));
+        $comments = [
+            'I ate a normal rock once. It did NOT taste like bacon!',
+            'Woohoo! I\'m going on an all-asteroid diet!',
+            'I like bacon too! Buy some from my site! bakinsomebacon.com',
+        ];
+
+        return $this->render('article/show.html.twig', [
+            'title' => ucwords(str_replace('-', ' ', $slug)),
+            'comments' => $comments,
+        ]);
     }
 }
